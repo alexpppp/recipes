c532454ae0d551bd6998d9a2c4fb75a1951ed147
Alex Procter <acoprocter@gmail.com>
2025-09-10 13:56:08 +1200

    test removing pathPrefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
-const isProd = process.env.ENV === "prod";
+// const isProd = process.env.ENV === "prod";
 
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('img');
     return {
       markdownTemplateEngine: "njk",
-      pathPrefix: isProd ? "/recipes/" : "/",  // Only apply in production for GH pages
+      // pathPrefix: isProd ? "/recipes/" : "/",  // Only apply in production for GH pages
       dir: {
         input: ".",
         output: "docs"  // GitHub Pages and local will serve from here
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Recipes
+
+Deploys to [https://alexpppp.github.io/recipes/](https://alexpppp.github.io/recipes/)
